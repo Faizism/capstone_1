3804d39ea1f894d70a9ed53036bba2ea59049ee4
Faiz <ismail1.faiz1@gmail.com>
2024-10-12 15:07:27 +0700

    Fixing read feature & Add basic create feature

--- main.py
+++ main.py
@@ -18,17 +18,57 @@ product = [{'id' : 'P01', 'nama produk' : 'Redmi 12', 'harga': 2_000_000, 'stok'
 
 # /===== Feature Program =====/
 # Create your feature program here
-def read():
+def display():
     print("Daftar Smartphone di Toko Berkah Jaya\n")
     print(f"{('id').ljust(10)}|{('Nama Smartphone').ljust(30)}|{('Harga').ljust(20)}|Stok")
     for item in product :
         print(f"{item['id'].ljust(10)}|{item['nama produk'].ljust(30)}|{str(item['harga']).ljust(20)}|{str(item['stok']).ljust(20)}")
-    return
+
+def read():
+    menu = input('''
+    Menu 1 : Menampilkan Data Smartphone
+    1. Menampilkan Seluruh Data Smartphone
+    2. Mencari Data HP Berdasarkan Harga
+    3. Kembali ke Menu Awal
+    Masukkan angka Menu yang ingin di jalankan: ''')
+    if menu == '1':
+        display()
+    elif menu == '2':
+        try:
+            min_price = int(input("Masukkan harga minimum: "))
+            max_price = int(input("Masukkan harga maksimum: "))
+            filtered_product = [item for item in product if min_price <= item['harga']<= max_price]
+            if filtered_product == []:
+                print(f"Smartphone pada rentang harga {min_price} - {max_price} Tidak Tersedia")
+            else:
+                print(filtered_product)
+        except ValueError:
+            print('Masukkan Harga Yang Benar')
+    elif menu == '3':
+        main()
+    else :
+        print('Menu Tidak Tersedia')
+    read()
 
 def create():
-    """Function for create the data
-    """
-    return
+    menu = input('''
+    Menu 2 : Menambahkan Daftar Smartphone
+    1. Menambah Data Smartphone Baru
+    2. Kembali ke Menu Awal
+    Masukkan angka Menu yang ingin di jalankan: ''')
+    if menu == '1':
+        add_name = input("Masukkan Merk Smartphone & Serinya: ")
+        add_price= input("Masukkan Harga per unit: ")
+        add_stock = input("Masukkan Stok: ")
+        new_id = f'P{int(product[-1]['id'][1:]) + 1 :02d}'
+        product.append({
+            'id':new_id,
+            'nama produk':add_name,
+            'harga': add_price,
+            'stok': add_stock
+        })
+        print("Data Smartphone Berhasil Ditambahkan")
+        display()
 
 def update():
     """Function for update the data
